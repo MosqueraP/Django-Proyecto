63f99ef69a7ec0cea0f1bd227927562094bee42b
Plinio <mcplinio.15@gmail.com>
2024-01-19 13:32:55 -0500

    Vista basada en clase primera parte

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from posts.views import dummy_view, status_code_view, entry_list, redirect_back_home
+from posts.views import dummy_view, status_code_view, entry_list, redirect_back_home, MyClassView
 
 app_name = 'posts'
 
 urlpatterns = [
-    path('', redirect_back_home, name='entry-list'),
+    path('', MyClassView.as_view(), name='entry-list'),
     path('<id>/', dummy_view, name='entry-detail'),
     # path('<?pid>/', dummy_view, name='entry-detail'),
     path('<id>/delete/', dummy_view, name='entry-delete'),
