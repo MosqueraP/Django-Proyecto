7210776674f47ece27c68e62c82628398ea22a8c
Plinio <mcplinio.15@gmail.com>
2024-01-19 14:39:40 -0500

    Authenticated

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from posts.views import dummy_view, status_code_view, entry_list, redirect_back_home, MyClassView
+from posts.views import dummy_view, status_code_view, entry_list, redirect_back_home, MyClassView, MyLisView 
 
 app_name = 'posts'
 
 urlpatterns = [
-    path('', MyClassView.as_view(), name='entry-list'),
-    path('<id>/', dummy_view, name='entry-detail'),
+    # path('', MyClassView.as_view(), name='entry-list'),
+    path('', MyLisView.as_view(), name='entry-list'), # con modelos de la
+    path('<int:id>/', dummy_view, name='entry-detail'),
     # path('<?pid>/', dummy_view, name='entry-detail'),
     path('<id>/delete/', dummy_view, name='entry-delete'),
     path('<id>/update/', dummy_view, name='entry-update'),
