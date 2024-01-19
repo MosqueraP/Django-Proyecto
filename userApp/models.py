005035b63e7a6d519ab870cacf905ae653fab7f1
Plinio <mcplinio.15@gmail.com>
2024-01-18 23:21:29 -0500

    Editar metodos y propiedases en modelos

--- userApp/models.py
+++ userApp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class User(models.Model):
     firs_name = models.CharField(max_length=30, verbose_name='Nombre')
     lasts_name = models.CharField(max_length=30, verbose_name='Apellido')
-    cars = models.ManyToManyField('car', verbose_name='Carros del usuario')
+    cars = models.ManyToManyField('Car', verbose_name='Carros del usuario')
 
     def __str__(self):
         return self.firs_name 
@@ -22,6 +23,35 @@ class Website(models.Model):
     reting = models.IntegerField()
     status = models.CharField(choices=STATUS_CHOISES, max_length=1)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    #Fue lanzado la ultima semana
+    def was_released_last_week(self):
+        if self.release_date < datetime(2023,6,6):
+            return 'lanzamiento antes de la semana pasada'
+        else:
+            return 'lanzamiento esta semana'
+        
+    @property
+    def get_full_name(self):
+        return f'Estes el nombre completo del sitio web: {self.ame}'
+
+    def __str__(self):
+        return self.name
+    
+    #busqueda de sitio por su ID
+    def get_absolute_url(self):
+        return f'/website/{self.id}'
+    
+    # sobre escribiendo metodo save
+    def save(self, *args, **kwargs):
+        print('Estamos guardando')
+        super().save( *args, **kwargs)
+
+    class Meta:
+        ordering = ['reting']
+        db_table = 'Nombre de la tabla'
+        verbose_name = 'sitio Web'
+        verbose_name_plural = 'sitio Webs'
 
 
 class Car(models.Model):
