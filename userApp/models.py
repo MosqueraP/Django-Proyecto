16e7ff41d99106b0506a4f85571a341933f45315
Plinio <mcplinio.15@gmail.com>
2024-01-18 20:05:31 -0500

    actualizar lso campos del modelo Website

--- userApp/models.py
+++ userApp/models.py
@@ -8,15 +8,16 @@ class User(models.Model):
     def __str__(self):
         return self.firs_name 
 
-# STATUS_CHOISES = (
-# 		('R', 'Reviewd'),
-# 		('N', 'Not Reviewd'),
-# 		('E', 'Error'),
-# 		('A', 'Acepted'),
-# )
+STATUS_CHOISES = (
+		('R', 'Reviewd'),
+		('N', 'Not Reviewd'),
+		('E', 'Error'),
+		('A', 'Accepted'),
+)
 class Website(models.Model):
     name = models.CharField(max_length=50)
     url = models.URLField()
     release_date = models.DateField()
     reting = models.IntegerField()
+    status = models.CharField(choices=STATUS_CHOISES, max_length=1)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
